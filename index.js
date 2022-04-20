44e98e99d0da01ba814f962afeaa3b3e70958fc3
Swarna Sarker <swarnaprony81@gmail.com>
2022-04-20 16:52:28 +0200

    Added Keypress and mouse click event

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function makeSound(key) {
             var audio = new Audio("sounds/tom-2.mp3");
             audio.play();  
             break;
-        case "s":
+        case "s":https://swarnaprony.github.io/PlayDrumKit/
             var audio = new Audio("sounds/tom-3.mp3");
             audio.play();  
             break;
